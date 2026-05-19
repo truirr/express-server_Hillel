aba2c827da0fe6dc1bfceafddb3f7ec6e802365a
Leil <18070508ao@gmail.com>
2026-05-19 19:21:40 +0300

    Use async bcrypt password hashing

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,46 +1,48 @@
-import crypto from 'crypto';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      required: [true, 'Name is required'],
       trim: true,
-      minlength: 2,
+      minlength: [2, 'Name must be at least 2 characters'],
     },
     email: {
       type: String,
-      required: true,
+      required: [true, 'Email is required'],
       unique: true,
       lowercase: true,
       trim: true,
+      index: true,
     },
-    passwordHash: {
+    password: {
       type: String,
-      required: true,
-    },
-    passwordSalt: {
-      type: String,
-      required: true,
+      required: [true, 'Password is required'],
+      minlength: [6, 'Password must be at least 6 characters'],
     },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
-userSchema.methods.setPassword = function setPassword(password) {
-  this.passwordSalt = crypto.randomBytes(16).toString('hex');
-  this.passwordHash = crypto
-    .pbkdf2Sync(password, this.passwordSalt, 10000, 64, 'sha512')
-    .toString('hex');
-};
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
 
-userSchema.methods.validatePassword = function validatePassword(password) {
-  const hash = crypto
-    .pbkdf2Sync(password, this.passwordSalt, 10000, 64, 'sha512')
-    .toString('hex');
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
-  return this.passwordHash === hash;
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
 const UserModel = mongoose.model('User', userSchema);
